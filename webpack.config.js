7a9c7db0b0ff15ae90a5a952a4aef84b67daf90b
WhiteCloudUU <jinzhao.chang.bayarea@gmail.com>
2021-03-24 20:17:57 -0700

    Push to Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 if (process.env.NODE_ENV === 'test') {
   require('dotenv').config({ path: '.env.test' });  
 } else if (process.env.NODE_ENV === 'development') {
-  
   require('dotenv').config({ path: '.env.development' });
 }
 
